4bd11e8807d1faf5ad0bd656343b17ce4db8c475
nuro <4991309+NuroDev@users.noreply.github.com>
2022-07-07 14:06:24 +0000

    Added Vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,16 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import UnoCSS from "unocss/vite";
 
+/**
+ * @see https://github.com/withastro/astro/tree/main/packages/integrations/vercel
+ */
+import VercelAdapter from "@astrojs/vercel/serverless";
+
 import PresetIcons from "@unocss/preset-icons";
 import PresetWind from "@unocss/preset-wind";
 
 export default defineConfig({
+  adapter: VercelAdapter(),
   integrations: [solidJs()],
   vite: {
     plugins: [
