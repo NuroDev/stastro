25ff2fd47570160c0d64af617cae8469efe34dbc
nuro <4991309+NuroDev@users.noreply.github.com>
2022-07-07 13:53:11 +0000

    Added UnoCSS

--- astro.config.ts
+++ astro.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
+import UnoCSS from "unocss/vite";
 
 export default defineConfig({
   integrations: [solidJs()],
+  vite: {
+    plugins: [UnoCSS({})],
+  },
 });
