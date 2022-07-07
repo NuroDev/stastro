9a8899dc4396ae3ef966aeedf5a883df50410b24
nuro <4991309+NuroDev@users.noreply.github.com>
2022-07-07 14:42:41 +0000

    Temp: Disabling Vercel adapter

--- astro.config.ts
+++ astro.config.ts
@@ -1,22 +1,20 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
-import sitemap from "@astrojs/sitemap";
+// import sitemap from "@astrojs/sitemap";
 import WindiCSS from "vite-plugin-windicss";
 
 /**
  * @see https://github.com/withastro/astro/tree/main/packages/integrations/vercel
  */
-import VercelAdapter from "@astrojs/vercel/serverless";
+// import VercelAdapter from "@astrojs/vercel/serverless";
 
 const isProduction = process.env.NODE_ENV === "production";
 
 export default defineConfig({
-  adapter: isProduction ? VercelAdapter() : undefined,
+  // adapter: isProduction ? VercelAdapter() : undefined,
   integrations: [
     solidJs(),
-    sitemap({
-      // ...
-    }),
+    // sitemap(),
   ],
   // site: process.env.VERCEL ? process.env.VERCEL_URL : undefined,
   vite: {
