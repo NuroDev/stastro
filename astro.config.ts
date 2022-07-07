a051768cafebe27fefb2e18c3eaadf45bc24b3a7
nuro <4991309+NuroDev@users.noreply.github.com>
2022-07-07 13:54:35 +0000

    Added Windi CSS preset

--- astro.config.ts
+++ astro.config.ts
@@ -2,9 +2,19 @@ import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
 import UnoCSS from "unocss/vite";
 
+import PresetWind from "@unocss/preset-wind";
+
 export default defineConfig({
   integrations: [solidJs()],
   vite: {
-    plugins: [UnoCSS({})],
+    plugins: [
+      UnoCSS({
+        presets: [
+          PresetWind({
+            dark: "class",
+          }),
+        ],
+      }),
+    ],
   },
 });
