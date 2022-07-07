bb45f4c9e6551ae6f6bc9647d0f51312651995ae
nuro <4991309+NuroDev@users.noreply.github.com>
2022-07-07 14:21:59 +0000

    Replaced UnoCSS with WindiCSS

--- astro.config.ts
+++ astro.config.ts
@@ -1,28 +1,16 @@
 import { defineConfig } from "astro/config";
 import solidJs from "@astrojs/solid-js";
-import UnoCSS from "unocss/vite";
+import WindiCSS from "vite-plugin-windicss";
 
 /**
  * @see https://github.com/withastro/astro/tree/main/packages/integrations/vercel
  */
 import VercelAdapter from "@astrojs/vercel/serverless";
 
-import PresetIcons from "@unocss/preset-icons";
-import PresetWind from "@unocss/preset-wind";
-
 export default defineConfig({
   // adapter: VercelAdapter(),
   integrations: [solidJs()],
   vite: {
-    plugins: [
-      UnoCSS({
-        presets: [
-          PresetIcons(),
-          PresetWind({
-            dark: "class",
-          }),
-        ],
-      }),
-    ],
+    plugins: [WindiCSS({})],
   },
 });
